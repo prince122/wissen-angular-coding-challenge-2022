76489352dd976c46e55c48947f8467588381e4b9
prince122 <vivekyadav8655@gmail.com>
2022-04-12 09:50:07 +0530

    1 file modified

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,19 +1,32 @@
 /**
  * Modify this file to fetch and display the login details
  */
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
-  selector: "app-welcome",
-  templateUrl: "./welcome.component.html",
-  styleUrls: ["./welcome.component.css"]
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
   user; // type this variable using user.type.ts file
   LoadingData = 'Load...';
-  constructor() {}
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userService.getUser().subscribe(
+      (data) => {
+        this.LoadingData = '';
+        this.user = data;
+      },
+      (err: Error) => {
+        this.LoadingData = err.message;
+      }
+    );
+  }
+
+  logout() {}
 
   ngOnDestroy() {}
 }
